b3ff429da2aaf57c9132dcc62d414b8dab1a45bb
subramanyam <saisubramanyam99@gmail.com>
2025-06-24 18:11:43 +0530

    added fun from ftr

--- entity/App.java
+++ entity/App.java
@@ -4,9 +4,14 @@ public class App{
     public static void main(String[] args) {
         System.out.println("Hello");
         greeting("Hey Good Morning!");
+        fun("sai");
     }
 
     public static void greeting(String s){
         System.out.println(s.toUpperCase());
+    }
+
+    public static void fun(String a){
+        System.out.println(a.repeat(2));
     }
 }
