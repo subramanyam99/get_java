708969b487956f0748d1cde1f903d1788d316db0
subramanyam <saisubramanyam99@gmail.com>
2025-06-24 17:49:46 +0530

    added greeting method from ftr

--- entity/App.java
+++ entity/App.java
@@ -3,5 +3,10 @@ package entity;
 public class App{
     public static void main(String[] args) {
         System.out.println("Hello");
+        greeting("Hey Good Morning!");
+    }
+
+    public static void greeting(String s){
+        System.out.println(s.toUpperCase());
     }
 }
